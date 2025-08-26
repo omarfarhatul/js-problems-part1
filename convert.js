f768a5b14ddcd99e4dbc729d8b7d066c2ad47369
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-08-26 22:56:24 +0600

    Add mile and km math

--- convert.js
+++ convert.js
@@ -5,7 +5,12 @@ function inchTofeet(inch){
 }
 function inchTofeet2(inch){
     const feetFrection = inch/12;
-    const feetNumber = perseInt(feetFrection);
+    const feetNumber = parseInt(feetFrection);
+    const inchRemaining = inch%12;
+    const result = feetNumber+'ft '+inchRemaining+' inch.'
+    return result;
 }
 const shuoHeight = inchTofeet(75);
 const shuoHeight2 = inchTofeet2(75);
+console.log(shuoHeight);
+console.log(shuoHeight2);
---
+++ mile-to-km.js
@@ -0,0 +1,16 @@
+function mileTokm(mile){
+    const kilo =mile*1.60934;
+    return kilo;
+}
+const km = mileTokm(2);
+console.log(km);
+
+
+
+function kmTomile(kilo){
+    const mile =kilo*0.621371;
+    return mile;
+}
+const mile = kmTomile(2);
+console.log(mile);
+f
