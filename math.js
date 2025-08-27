d07524981746e780616ad28e9ece6d07ed55f3e6
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-08-27 18:23:56 +0600

    Add some maths

---
+++ math.js
@@ -0,0 +1,17 @@
+const min = Math.min(34, 64, 23, 75, 12, 36);
+console.log(min);
+
+const max = Math.max(34, 64, 23, 75, 12, 36);
+console.log(max);
+
+console.log(Math.PI);
+console.log(Math.abs(10-5));
+console.log(Math.round());
+console.log(Math.round(2.25));
+console.log(Math.round(2.86));
+console.log(Math.floor(2.999));
+console.log(Math.ceil(2.996));
+console.log(Math.random());
+
+const rand=(Math.random()*10);
+console.log(rand);
